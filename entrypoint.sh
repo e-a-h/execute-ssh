b38742932b1d94e96140a2f0f4211a563137a6a3
AEnterprise <aenterprise2@gmail.com>
2019-08-26 18:24:08 +0200

    another connection attempt

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@ SSHPATH="$HOME/.ssh"
 mkdir "$SSHPATH"
 echo "$DEPLOY_KEY" > "$SSHPATH/key"
 chmod 600 "$SSHPATH/key"
-sh -c "ssh -v -o StrictHostKeyChecking=no -i $SSHPATH/key -J $SERVER_CONNECTION_STRING   \"$COMMAND\""
+ssh -o StrictHostKeyChecking=no -v -i "$SSHPATH/key" -J  "$SERVER_CONNECTION_STRING" "$COMMAND"
+
+
